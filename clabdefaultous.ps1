bb31707b7b4f39133bc5006e0d12059dbeda8872
Clayton Pinchot <cpinchot@swbell.net>
2026-05-27 16:39:24 -0500

    Upload Powershell script for OU creation
    Creation of a Powershell script that creates OUs in active directory from a csv file

---
+++ clabdefaultous.ps1
@@ -0,0 +1,15 @@
+#clabdefaultous.ps1
+#Clayton Pinchot
+#Creates a default OUS structure for a new domain
+#This script is meant to be run on a domain controller in the new domain
+
+#Import AD Module
+Import-Module ActiveDirectory
+
+#Import csv file with OU names and descriptions
+$ous = Import-Csv -Path "https://raw.githubusercontent.com/clayton-pinchot/AD-Windows-Server-Homelab/refs/heads/main/OU-Creation-1.csv"
+
+#loop through each OU in the csv file and create it in AD
+foreach ($ou in $ous){
+    New-ADOrganizationalUnit -Name $ou.ouName -Path $ou.ouPath
+}
